09f03bab5783bd745c231890f6a878fff8f9bfb4
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-08-01 21:08:03 +0700

    Rewrite-Merge from gaby preparation

--- src/control.hh
+++
@@ -1,8 +0,0 @@
-#ifndef CONTROL_HPP
-#define CONTROL_HPP
-
-void MouseEvent(int, int, int, int);
-void KeyboardDownEvent(unsigned char, int, int);
-void KeyboardUpEvent(unsigned char, int, int);
-
-#endif
