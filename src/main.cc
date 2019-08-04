6c7bc37fe27bbc3bf170c0e17b56c99cea41540e
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-08-04 22:08:29 +0700

    Titik Done

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,16 @@
+#include <iostream>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <vector>
 #include <cmath>
+
+typedef struct {
+    GLfloat x;
+    GLfloat y;
+} koordinat;
+
+std::vector<koordinat> data_koordinat;
 
 void lingkaran(float TITIK_LAYAR_X, float TITIK_LAYAR_Y, float LEBAR_X, float LEBAR_Y, float JUMLAH_SUDUT, float ROTASI)
 {
@@ -20,28 +29,51 @@ void lingkaran(float TITIK_LAYAR_X, float TITIK_LAYAR_Y, float LEBAR_X, float LE
 void myDisplay()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0, 0, 0);
+	for(int i=0; i<data_koordinat.size(); i++)
+	{
+		glPointSize(5.0);
+		glBegin(GL_POINTS);
+		glVertex2f(data_koordinat[i].x, data_koordinat[i].y);
+		glEnd();
+	}
+
     glBegin(GL_LINE_LOOP);
         glColor3f(1.0, 1.0, 0);
-        glVertex2i(15, 40);
-        glVertex2i(30, 40);
-        glVertex2i(30, 60);
-        glVertex2i(15, 60);
+        glVertex2i(150, 400);
+        glVertex2i(180, 400);
+        glVertex2i(180, 430);
+        glVertex2i(150, 430);
     glEnd();
 
     glBegin(GL_LINE_LOOP);
         glColor3f(1, 0, 0);
-        glVertex2i(40, 40);
-        glVertex2i(55, 40);
-        glVertex2i(47, 60);
+        glVertex2i(400, 400);
+        glVertex2i(430, 400);
+        glVertex2f(411, 430);
     glEnd();
 
     glColor3f(0, 1, 0);
     for(int i = 0; i < 35; i++)
 	{
-		lingkaran(20, 25, 20, 20, 1000, 20);
+		lingkaran(400, 400, 20, 20, 1000, 20);
 	}
 
     glutSwapBuffers();
+}
+
+void myMouse (int button, int state, int mousex, int mousey)
+{
+	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		koordinat mouseKoord;
+
+		mouseKoord.x = mousex;
+		mouseKoord.y = 600 - mousey;
+		data_koordinat.push_back(mouseKoord);
+	}
+
+	glutPostRedisplay();
 }
 
 int main(int argc, char** argv)
@@ -52,7 +84,8 @@ int main(int argc, char** argv)
     glutCreateWindow("Tugas Rancang");
     glLineWidth(5.0);
     glutDisplayFunc(myDisplay);
-    gluOrtho2D(0, 100, 0, 100);
+    glutMouseFunc(myMouse);
+    gluOrtho2D(0, 800, 0, 600);
     glClearColor(1, 1, 1, 1);
 
     glutMainLoop();
