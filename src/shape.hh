3cb4cc73d00f8fc997976fa1743627423469230c
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-08-07 15:00:25 +0700

    added licence notice to all source file

--- src/shape.hh
+++ src/shape.hh
@@ -1,3 +1,17 @@
+/* defining implementation of entities shapes, and vertices
+ * Copyright (C) 2019  Erlangga Ibrahim, Aurelia Gabriele
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef SHAPE_HH
 #define SHAPE_HH
 
